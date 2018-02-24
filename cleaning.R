5cbbe14e4f8155eb3e9491d3b9b77a312960207d
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2018-02-24 11:56:41 -0500

    address and date cleanup

--- cleaning.R
+++ cleaning.R
@@ -1,5 +1,7 @@
 library(stringr)
+library(RJSONIO)
 
+Sys.setlocale('LC_ALL','C')
 setwd("~/hackathons/datachallenge2018")
 
 passengers <- read.csv("data/PassengerData.csv")
@@ -8,8 +10,43 @@ events <- read.csv("data/EventTable.csv")
 
 date.standardize <- function(raw_date) {
   date_string <- as.character(raw_date)
-  matches <- str_match(date_string, "^([0-9])\\/\\s*\\/")
-  print(matches)
+  matches_yr <- str_match(date_string, "^([0-9]+)\\/\\s*\\/\\s*$")
+  matches_mn <- str_match(date_string, "^([0-9]+)\\/([0-9]+)\\/\\s*$")
+
+  if(!is.na(matches_yr[1,2])) {
+    return(paste(as.character(matches_yr[1,2]), "01", "01", sep="/"))
+  } else if(!is.na(matches_mn[1,2]) && !is.na(matches_mn[1,3])) {
+    return(paste(as.character(matches_mn[1,2]), as.character(matches_mn[1,3]), "01", sep="/"))
+  } else {
+    matches <- str_match(date_string, "^([0-9]+)\\/\\s*\\/\\s*$")
+    return(date_string)
+  }
 }
 
-sapply(events[,3], date.standardize)
+coords.from.address <- function(event.place.cols) {
+  address <- paste(event.place.cols[1,1], event.place.cols[1,2], event.place.cols[1,3])
+  clean_address <- iconv(address, to='ASCII//TRANSLIT')
+  return(geocodeAddress(clean_address))
+}
+
+geocodeAddress <- function(address) {
+  url <- "http://maps.google.com/maps/api/geocode/json?address="
+  url <- URLencode(paste(url, address, "&sensor=false", sep = ", "))
+  x <- fromJSON(url, simplify = FALSE)
+  if (x$status == "OK") {
+    out <- c(x$results[[1]]$geometry$location$lng,
+             x$results[[1]]$geometry$location$lat)
+  } else {
+    out <- NA
+  }
+  Sys.sleep(0.2)  # API only allows 5 requests per second
+  out
+}
+
+events[,"StartDate"] <- sapply(events[,"StartDate"], date.standardize)
+events[,"EndDate"] <- sapply(events[,"EndDate"], date.standardize)
+for(i in 1:nrow(events)) {
+  events[i,c("Longitude", "Latitude")] <- coords.from.address(events[i,c("EventPlaceCurrent", "EventStateCurrent", "EventCountryCurrent")])
+  
+}
+write.csv(events, "events_clean.csv")
